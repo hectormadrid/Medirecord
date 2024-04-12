50de94462b674b42f9f7cfd2762add2bf17a0d86
hectormadrid <hectorfmf2002@gmail.com>
2024-04-12 14:24:16 -0400

    el programa finaliza cuando todos los pacientes  respondan

--- src/programador.js
+++ src/programador.js
@@ -7,12 +7,14 @@ const MSG_ASISTIRA = 'Perfecto, lo esperamos mañana';
 const MSG_NO_ASISTIRA = 'Gracias por responder';
 
 let respuestas = 0;
+let pacientesSinResponder = 0; // Variable para llevar un registro de los pacientes que no han respondido
 let connection;
 
 async function obtenerPacientes() {
     try {
         const [rows] = await connection.execute('SELECT rut, telefono FROM paciente');
         if (rows && rows.length > 0) {
+            pacientesSinResponder = rows.length; // Inicializar con la cantidad de pacientes
             return rows.filter(row => validarNumeroTelefono(row.telefono));
         } else {
             console.log('No se encontraron pacientes en la consulta.');
@@ -28,18 +30,21 @@ async function actualizarEstadoPaciente(rut, respuesta) {
     let estado = '';
 
     if (respuesta === 'si') {
-        estado = 'Asistirá';
+        estado = 'asistirá';
     } else if (respuesta === 'no') {
-        estado = 'No Asistirá';
+        estado = 'no asistirá';
     }
 
     try {
         const [result] = await connection.execute('UPDATE hora SET Asistencia = ? WHERE Rut_Paciente = ?', [estado, rut]);
         if (result && result.affectedRows > 0) {
             console.log(`Estado del paciente con RUT ${rut} actualizado a ${estado}`);
+            pacientesSinResponder--; // Decrementar la cantidad de pacientes sin responder
         } else {
             console.log(`No se pudo actualizar el estado del paciente con RUT ${rut}`);
         }
+
+    
     } catch (error) {
         console.log('Error al actualizar el estado del paciente en la base de datos:', error);
     }
@@ -48,7 +53,7 @@ async function actualizarEstadoPaciente(rut, respuesta) {
 async function programador_tareas(cliente) {
     connection = await conectarBaseDatos();
 
-    const tiempo = '0 11 20 * * *';
+    const tiempo = '0 16 14 * * *';
 
     if (cron.validate(tiempo)) {
         console.log('Cron inicializado');
@@ -93,14 +98,14 @@ async function manejarRespuesta(cliente, message) {
             } else {
                 console.log('No se encontró el paciente en la base de datos');
             }
-        } catch (error) {
-            console.log('Error en la consulta a la base de datos:', error);
-        }
-
-        if (respuestas === 1) {
-            console.log('Aplicación finalizada');
+                // Verificar si todos los pacientes han respondido
+        if (pacientesSinResponder === 0) {
+            console.log('Todos los pacientes han respondido. Aplicación finalizada');
             cliente.destroy();
             process.exit();
+        }
+        } catch (error) {
+            console.log('Error en la consulta a la base de datos:', error);
         }
     }
 }
