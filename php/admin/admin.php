a2bfc6c8d10a2a9b014cc2908ea43ca89e882694
hectormadrid <hectorfmf2002@gmail.com>
2024-04-30 02:16:01 -0400

    menu

--- php/admin/admin.php
+++ php/admin/admin.php
@@ -3,9 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/boxicons@2.1.3/dist/boxicons.js"></script>
+    <link href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <title>MediRecord</title>
 </head>
 <body>
+
+<nav class="nav">
+    <ul class="list">
+      <li class="list_item">
+        <div class="list_botton">
+          <box-icon name='clinic' class="imagen lista"></box-icon>
+          <a href="#" class="link inicio">Inicio </a>
+        </div>
+      </li>
+      
+      <li class="list_item"></li>
+      <div class="list_botton--click ">
+      <box-icon type='solid' name='book'></box-icon>
+      <a href="#" class="link reportes">Reportes</a>
+      <box-icon name='chevron-right' ></box-icon>
+      </div>
+    </ul>
+  </nav>
+  <div class="container mx-auto px-4">
      <h1>holaaaa</h1>
 </body>
 </html>
--- php/user/home.php
+++ php/user/home.php
@@ -24,11 +24,11 @@ if (!isset($_SESSION['correo'])) {
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
   <!-- Tu CSS personalizado -->
-  <link rel="stylesheet" href="css/Estilos.css">
-
+  <link rel="stylesheet" href="../../css/Menu.css">
+  <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
   <!-- jQuery -->
   <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-
+  <script src="https://unpkg.com/boxicons@2.1.3/dist/boxicons.js"></script>
   <!-- DataTables JS -->
   <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
 
@@ -47,82 +47,177 @@ if (!isset($_SESSION['correo'])) {
 
 <body class="bg-gray-100 text-gray-900 tracking-wider leading-normal ">
 
-  <div class="container mx-auto px-4">
-
-    <h1 class="text-4xl md:text-5xl text-center font-serif font-bold text-black-500 mb-6 mt-6">
-      Bienvenido al Sistema de Recordatorio de Citas Médicas
-    </h1>
-
-
-    <div class="container mx-auto max-w-lg mt-20 text-center">
-      <h2 class="text-3xl font-semibold text-gray-800 mb-8">¡Bienvenido, <?php echo $_SESSION['correo']; ?>!</h2>
-      <a href="../Inicio_sesion.php" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Cerrar sesión</a>
+  <div class="sidebar close">
+    <div class="logo-details">
+      <box-icon name='user-circle' color="#ffffff" class="mr-3 ml-2"></box-icon>
+      <span class="logo_name text-center" style='color:#ffffff'>Administrador</span>
     </div>
+    <ul class="nav-links">
+      <li>
+        <a href="#">
+          <i class='bx bx-grid-alt'></i>
+          <span class="link_name">Panel</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Inicio</a></li>
+        </ul>
+      </li>
+      <li>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class='bx bx-book-alt'></i>
+            <span class="link_name">Reportes</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow'></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Productos</a></li>
+          <li><a href="#">Sub Menú 1</a></li>
+          <li><a href="#">Sub Menú 2</a></li>
+        </ul>
+      </li>
+     
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class='bx bxs-user-voice'></i>
+            <span class="link_name">Clientes</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow'></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Clientes</a></li>
+          <li><a href="#">Sub Menú 1</a></li>
+          <li><a href="#">Sub Menú 2</a></li>
+          <li><a href="#">Sub Menú 3</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bxs-contact'></i>
+          <span class="link_name">Contactos</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Contactos</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bxs-edit-location'></i>
+          <span class="link_name">Sedes</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Sedes</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class='bx bx-cog'></i>
+          <span class="link_name">Configuración</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Configuración</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="profile-details">
 
-    <div class="cargaExel bg-blue-100 border-b-4 border-gray-500 rounded-lg p-4 md:p-6 shadow-md relative ">
-      <label for="excelFile" class="block text-lg font-semibold mb-2">Cargar archivo Excel:</label>
-      <input class="block w-full py-2 px-3 md:px-4 mb-3 md:mb-4 rounded-md border border-gray-300" type="file" id="excelFile" accept=".xls, .xlsx">
-      <button onclick="cargarExcel()" class=" static block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-3 md:px-4 rounded mb-2 md:mb-0 md:mr-2">
-        Cargar Excel
-      </button>
-      <button class="block w-full md:w-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 md:px-4 absolute bottom-6 right-6 rounded " id="guardarDatosBtn">
-        Enviar Mensaje
-
-      </button>
-    </div>
-
-
-
-    <div class="flex items-center justify-center font-sans font-bold text-center md:text-left break-normal text-indigo-500 px-2 py-4 md:py-8 text-xl md:text-2xl">
-      <p>Lista de Pacientes</p>
-    </div>
-
-    <div class="overflow-x-auto">
-      <table id="tabla_pacientes" class="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
-        <thead class="bg-blue-500 text-white">
-          <tr>
-            <th scope="col" class="px-6 py-3 text-left font-bold">Codigo</th>
-            <th scope="col" class="px-6 py-3 text-left font-bold">Numero</th>
-            <th scope="col" class="px-6 py-3 text-left font-bold">Nombre</th>
-            <th scope="col" class="px-6 py-3 text-left font-bold">Rut</th>
-            <th scope="col" class="px-6 py-3 text-left font-bold">Dia</th>
-            <th scope="col" class="px-6 py-3 text-left font-bold">Hora</th>
-            <th scope="col" class="px-6 py-3 text-left font-bold">Medico</th>
-            <th scope="col" class="px-6 py-3 text-left font-bold">Especialidad</th>
-            <th scope="col" class="px-6 py-3 text-left font-bold">Tipo de Consulta</th>
-            <th scope="col" class="px-6 py-3 text-left font-bold">Lugar</th>
-            <th scope="col" class="px-6 py-3 text-left font-bold">Recinto</th>
-            <th scope="col" class="px-6 py-3 text-left font-bold">Email</th>
-          </tr>
-        </thead>
-        <tbody class="bg-blue-100 divide-y divide-blue-200">
-          <!-- Aquí irán tus datos -->
-        </tbody>
-      </table>
-
-    </div>
-
+          <div class="name-job  text-wrap overflow-hidden ">
+            <div class="profile_name  " >
+              Usuario,  <?php echo $_SESSION['correo']; ?>!</div>
+          </div>
+          <i class='bx bx-log-out'></i>
+        </div>
+      </li>
+    </ul>
   </div>
+  <section class="home-section  overflow-y-auto ">
+    <div class="home-content">
+      <i class='bx bx-menu'></i>
+      <span class="text">Menu</span>
+    </div>
 
-  <div class="modal fade" id="filasOmitidasModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Filas Omitidas</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div class="container mx-auto px-4">
+
+      <h1 class="  text-4xl md:text-5xl text-center font-serif font-bold text-black-500 mb-6 mt-6">
+        Bienvenido al Sistema de Recordatorio de Citas Médicas
+      </h1>
+
+
+      <div class="container mx-auto max-w-lg mt-20 text-center">
+        <h2 class="text-3xl font-semibold text-gray-800 mb-8">¡Bienvenido, <?php echo $_SESSION['correo']; ?>!</h2>
+        <a href="../Inicio_sesion.php" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mb-4 ">Cerrar sesión</a>
+      </div>
+
+      <div class="container mx-auto px-4 relative">
+        <div class="cargaExel bg-blue-100 border-b-4 border-gray-500 rounded-lg p-4 md:p-6 shadow-md ">
+          <label for="excelFile" class="block text-lg font-semibold mb-2">Cargar archivo Excel:</label>
+          <input class="block w-full py-2 px-3 md:px-4 mb-3 md:mb-4 rounded-md border border-gray-300" type="file" id="excelFile" accept=".xls, .xlsx">
+          <button onclick="cargarExcel()" class="static block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-3 md:px-4 rounded mb-2 md:mb-0 md:mr-2">
+            Cargar Excel
+          </button>
+          <button class="block w-full md:w-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 md:px-4 absolute  right-10 top-28 rounded" id="guardarDatosBtn">
+            Enviar Mensaje
+          </button>
         </div>
-        <div class="modal-body">
-          <ul id="filasOmitidasList"></ul>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+      </div>
+
+
+
+
+      <div class="flex items-center justify-center font-sans font-bold text-center md:text-left break-normal text-indigo-500 px-2 py-4 md:py-8 text-xl md:text-2xl">
+        <p>Lista de Pacientes</p>
+      </div>
+
+      <div class="overflow-x-auto">
+        <table id="tabla_pacientes" class="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
+          <thead class="bg-blue-500 text-white">
+            <tr>
+              <th scope="col" class="px-6 py-3 text-left font-bold">Codigo</th>
+              <th scope="col" class="px-6 py-3 text-left font-bold">Numero</th>
+              <th scope="col" class="px-6 py-3 text-left font-bold">Nombre</th>
+              <th scope="col" class="px-6 py-3 text-left font-bold">Rut</th>
+              <th scope="col" class="px-6 py-3 text-left font-bold">Dia</th>
+              <th scope="col" class="px-6 py-3 text-left font-bold">Hora</th>
+              <th scope="col" class="px-6 py-3 text-left font-bold">Medico</th>
+              <th scope="col" class="px-6 py-3 text-left font-bold">Especialidad</th>
+              <th scope="col" class="px-6 py-3 text-left font-bold">Tipo de Consulta</th>
+              <th scope="col" class="px-6 py-3 text-left font-bold">Lugar</th>
+              <th scope="col" class="px-6 py-3 text-left font-bold">Recinto</th>
+              <th scope="col" class="px-6 py-3 text-left font-bold">Email</th>
+            </tr>
+          </thead>
+          <tbody class="bg-blue-100 divide-y divide-blue-200">
+            <!-- Aquí irán tus datos -->
+          </tbody>
+        </table>
+
+      </div>
+
+    </div>
+
+
+    <div class="modal fade" id="filasOmitidasModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Filas Omitidas</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <ul id="filasOmitidasList"></ul>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-
+  </section>
   <script src="../../js/cargar.js"></script>
-
+  <script src="../../js/Menu_desplegable.js"></script>
 
   <?php
   include('../cargabd.php');
